27886bfcc551053e93470be0373754e268351651
Zach Smith <zachsmith258@gmail.com>
2025-04-13 10:34:39 -0400

    updated AU url

--- src/shared/educations.ts
+++ src/shared/educations.ts
@@ -3,6 +3,7 @@ import { Experience } from './interfaces/Experience';
 export const educations: Experience[] = [
   {
     name: 'Augusta University',
+    url: 'https://www.augusta.edu/',
     descriptions: [
       'Bachelor of Science in Information Technology',
       'Minor in Computer Science',
